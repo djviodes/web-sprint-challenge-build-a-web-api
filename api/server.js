e9b60c5c2a3ee6cd125358da0471731d40395187
djviodes <djviodes26@gmail.com>
2020-12-04 07:57:49 -0800

    Set up index.js and server.js

--- api/server.js
+++ api/server.js
@@ -1,7 +1,15 @@
 const express = require('express');
+
+const actionRouter = require('./actions/actions-router');
+const projectRouter = require('./projects/projects-router');
+
 const server = express();
 
-// Complete your server here!
-// Do NOT `server.listen()` inside this file!
+server.use('/actions', actionRouter);
+server.use('/projects', projectRouter);
+
+server.get('/', (req, res) => {
+    res.send('Welcome to the API');
+});
 
 module.exports = server;
